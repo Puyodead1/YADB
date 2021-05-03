28897c11ef2d1846d5e7b17c0b392b32bb093d97
Puyodead1 <23562356riley@gmail.com>
2021-05-02 20:51:15 -0400

    Bulk update interaction commands on ready
    + All interaction commands are loaded from ``interactionCommands.json`` and loaded in bulk in the on ready event.

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,44 +1,12 @@
 import CClient from "../CClient";
+import interactionCommands from "../interactionCommands.json";
 
-export default (client: CClient) => {
+export default async (client: CClient) => {
   console.log(`[Ready] Logged in as ${client.user?.tag}.`);
 
   client.mentionPrefix = new RegExp(`^<@!?${client.user?.id}>`);
 
-  client.guilds.cache.get("638455519652085780")?.commands.create({
-    name: "git",
-    description: "Gets current git branch",
-    options: [
-      {
-        type: 5,
-        name: "ephemeral",
-        description: "Whether the reply should be ephemeral",
-        required: false,
-      },
-    ],
-  });
-
-  client.guilds.cache.get("638455519652085780")?.commands.create({
-    name: "ping",
-    description: "Gets current bot ping",
-  });
-
-  client.guilds.cache.get("638455519652085780")?.commands.create({
-    name: "eval",
-    description: "Evaluates code",
-    options: [
-      {
-        type: 3,
-        name: "code",
-        description: "Code to evaluate",
-        required: true,
-      },
-      {
-        type: 5,
-        name: "ephemeral",
-        description: "Whether the reply should be ephemeral",
-        required: false,
-      },
-    ],
-  });
+  await client.guilds.cache
+    .get("638455519652085780")
+    ?.commands.set(interactionCommands);
 };
